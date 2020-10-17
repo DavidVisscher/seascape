b10261f14fa3710d77b309449906539304522d27
Marten/Qqwy <w-m@wmcode.nl>
2020-10-17 18:55:58 +0200

    Adding dockerignores to halve docker context size.

--- build_web.sh
+++ build_web.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-docker build seascape_umbrella/ -f seascape_umbrella/Dockerfile.web-t qqwy/seascape:web \
+docker build seascape_umbrella/ -f seascape_umbrella/Dockerfile.web -t qqwy/seascape:web \
     && docker push qqwy/seascape:web
