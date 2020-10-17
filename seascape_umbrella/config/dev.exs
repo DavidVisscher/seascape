77dc5ab1a770eefbcae0fb0a593fd4fbbbbb2d12
Marten/Qqwy <w-m@wmcode.nl>
2020-10-17 21:46:49 +0200

    fix mnesia config for dev

--- seascape_umbrella/config/dev.exs
+++ seascape_umbrella/config/dev.exs
@@ -74,3 +74,4 @@ config :phoenix, :plug_init_mode, :runtime
 # in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
 
+config :mnesia, dir: to_charlist(System.get_env("MNESIA_DIR", "./priv/mnesia/"))
